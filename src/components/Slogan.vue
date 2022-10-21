0253093fc6753cc6499d2907a7374d8c0663f99c
huasheng <h1785222224@qq.com>
2022-10-21 23:35:03 +0800

    test

--- src/components/Slogan.vue
+++ src/components/Slogan.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="slogan">
-    <span id="title">圣诞快乐 梦雨</span><br/>
-    <span id="from">To.Huasheng</span>
+    <span id="title">生日快乐</span><br>
+    <span id="from" @click="test" >     芝芝仙女 2022年10月23日</span>
+    <audio id="audio" src="/static/audio/cyx.mp3" controls="controls" hidden="hidden" autoplay="autoplay"  loop="loop"> </audio>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "Slogan"
-    }
+  export default {
+    name: "Slogan",
+          methods: {
+          test(){
+            let vo = document.getElementById("audio")
+            vo.autoplay = true
+            vo.play()
+          }
+      }
+  }
 </script>
 
 <style>
@@ -17,6 +25,10 @@
     text-align: center;
   }
   #title {
+    color: white;
+    font-size: 3em;
+  }
+  #titl {
     color: white;
     font-size: 3em;
   }
